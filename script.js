0137def1efa1b60247fd04aa749d1360a761c33b
CatSmith1 <105733850+CatSmith1@users.noreply.github.com>
2022-08-06 14:16:50 +0100

    Add CYF Blocks JS

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 /* paste the JavaScript code from the 'Generated Code' section of the CYF Blocks editor here */
+var image_links;
+
+
+image_links = ['https://i.imgur.com/JNiwSEm.jpg', 'https://i.imgur.com/3JKE9vI.jpg', 'https://i.imgur.com/487FZuU.jpg', 'https://i.imgur.com/bShtsex.jpg', 'https://i.imgur.com/POGQrTv.jpg', 'https://i.imgur.com/fCbBBhs.jpg'];
+
+
+document.getElementById('next-button').addEventListener('click', (event) => {
+  let element_images = document.getElementById('images');
+  image_links.push(image_links.shift());
+  element_images.setAttribute("src", image_links[0]);
+
+});
+
+document.getElementById('previous-button').addEventListener('click', (event) => {
+  let element_images2 = document.getElementById('images');
+  image_links.unshift(image_links.pop());
+  element_images2.setAttribute("src", image_links[0]);
+
+});
